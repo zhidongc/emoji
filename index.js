bd583d4587a453351009498df0045b983de81e77
zhidongc <51006904+zhidongc@users.noreply.github.com>
2022-02-04 12:16:59 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Interactive from './interactive/Interactive';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Interactive);
+//AppRegistry.registerComponent(appName, () => LightingExample);
+
---
+++ interactive/src/systems/index.js
@@ -0,0 +1,5 @@
+
+import Plane from './plane';
+import Obstacle from './obstacle';
+import Physics from './physics';
+export default [Physics, Plane, Obstacle];
